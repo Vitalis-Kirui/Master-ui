706d7cc7223f8f8a64a89a5b9a1262bf4f6844cb
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-24 21:37:19 +0300

    adding required components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { SinglePostComponent } from './Components/single-post/single-post.component';
+import { VerifyComponent } from './Components/verify/verify.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent}
+  {path:"", component:HomeComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"login", component:LoginComponent},
+  {path:"verify", component:VerifyComponent},
+  {path:"single-post", component:SinglePostComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { VerifyComponent } from './Components/verify/verify.component';
+import { SinglePostComponent } from './Components/single-post/single-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    VerifyComponent,
+    SinglePostComponent
   ],
   imports: [
     BrowserModule,
