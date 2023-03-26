9ff36237b37e908ec11375914a97d05371da5e9e
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-26 16:09:36 +0300

    switching to login from register page

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  // Go to login page
+  gotologin(){
+    this.router.navigate(['login']);
+  }
 
   ngOnInit(): void {
   }
