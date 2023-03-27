5939650ced24812631bdeeb39e5a5a7ccec88541
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-27 20:08:17 +0300

    conditionally displaying navbar links depending on log in status

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service : UserService) { }
 
   ngOnInit(): void {
+  }
+
+  // Logout user
+  logout(){
+    localStorage.removeItem('token');
+    window.location.reload();
   }
 
 }
