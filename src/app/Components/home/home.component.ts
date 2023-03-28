d84b94bba0e39b64396447eaaf485d497f1c8518
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-28 14:47:03 +0300

    making a post request for creating a new post

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PostService } from 'src/app/Services/post.service';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +11,38 @@ export class HomeComponent implements OnInit {
   // Form Variables
   postForm!: FormGroup;
 
-  constructor(private fbService: FormBuilder) {}
+  constructor(
+    private fbService: FormBuilder,
+    private postservice: PostService
+  ) {}
 
   ngOnInit() {
     // Form model
     this.postForm = this.fbService.group({
-      post: [''],
+      caption: [''],
       image: [''],
     });
   }
 
   // Submit
   createPost() {
-    console.log(this.postForm.value);
+
+    // Create a FormData object and add the form data
+  const formData = new FormData();
+  formData.append('caption', this.postForm.get('caption')?.value);
+  formData.append('image', this.postForm.get('image')?.value);
+
+  // Call the createPost() method in the PostService to send the post data to the server
+  this.postservice.createpost(formData).subscribe(
+    (response) => {
+      console.log('Post created successfully:', response);
+      // Reset the form after the post is created
+      this.postForm.reset();
+    },
+    (error) => {
+      console.error('Error creating post:', error);
+    }
+  );
+
   }
 }
--- src/app/Services/post.service.ts
+++ src/app/Services/post.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  // Creating a post request
+  createpost(post : any){
+    return this.http.post<any>(environment.newpost, post);
+  }
+
 }
